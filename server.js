246606b8be2048743fa5c393b95a2842bd9cbd45
horlard <oladipuposaulih@gmail.com>
2020-06-02 00:55:28 +0100

    fix: personalised the server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
- 
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
 server.use(router)
 server.listen(process.env.PORT || 3002, () => {
   console.log('JSON Server is running')
